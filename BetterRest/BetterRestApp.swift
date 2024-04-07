60949c5deba83780304e2149101f63a9277bb28f
HiteshMadaan28 <hiteshmadaan28@gmail.com>
2024-04-07 20:09:26 +0530

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Hitesh Madaan on 07/04/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
